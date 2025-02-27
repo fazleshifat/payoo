0d2171b87aeeec06196cdf034ea7746840cb2bc0
fazleshifat <mailbox189.shifat@gmail.com>
2025-02-27 20:09:11 +0600

    added reusable functions

--- scripts/cashout.js
+++ scripts/cashout.js
@@ -1,57 +1,57 @@
-document.getElementById('cashOutToggle').addEventListener('click', function (event) {
-    event.preventDefault();
-    const cashOutToggle = document.getElementById('cashoutSection');
-    const addMoneySection = document.getElementById('addMoneySection');
-    cashOutToggle.classList.remove('hidden');
-    addMoneySection.classList.add('hidden');
-    console.log('cashout clicked')
-})
+document.getElementById('cashOutToggle')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const cashOutToggle = document.getElementById('cashoutSection');
+        const addMoneySection = document.getElementById('addMoneySection');
+        cashOutToggle.classList.remove('hidden');
+        addMoneySection.classList.add('hidden');
+        console.log('cashout clicked')
+    })
 
 const cashOut = document.getElementById('btn-cashOut')
-.addEventListener('click', function(event){
-    event.preventDefault();
-    console.log('ahaha clicked')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
 
-    // Main balance
-    const mainBalance = document.getElementById('mainBalance').innerText;
-    const convertedMainBalance = parseFloat(mainBalance);
-    // console.log(convertedMainBalance, typeof convertedMainBalance);
+        // Main balance
+        const mainBalance = document.getElementById('mainBalance').innerText;
+        const convertedMainBalance = parseFloat(mainBalance);
+        // console.log(convertedMainBalance, typeof convertedMainBalance);
 
-    // Amount Feild
-    const cashoutAmount = document.getElementById('cashoutAmount').value;
-    const convertedCashoutAmount = parseFloat(cashoutAmount);
-    // console.log(convertedAmount, typeof convertedAmount);
+        // Amount Feild
+        const cashoutAmount = document.getElementById('cashoutAmount').value;
+        const convertedCashoutAmount = parseFloat(cashoutAmount);
+        // console.log(convertedAmount, typeof convertedAmount);
 
-    // Pin number feild
-    const cashoutPin = document.getElementById('cashoutPin').value;
-    const convertedCashoutPin = parseInt(cashoutPin);
-    // console.log(convertedPin, typeof convertedPin);
+        // Pin number feild
+        const cashoutPin = document.getElementById('cashoutPin').value;
+        const convertedCashoutPin = parseInt(cashoutPin);
+        // console.log(convertedPin, typeof convertedPin);
 
-    // accessing element for appending items
-    // const newInvalidAcc = document.getElementById('invalidAccount');
-    const newInvalidAmount = document.getElementById('cashoutInvalidAmount');
-    const newInvalidPin = document.getElementById('cashoutInvalidPin');
+        // accessing element for appending items
+        // const newInvalidAcc = document.getElementById('invalidAccount');
+        const newInvalidAmount = document.getElementById('cashoutInvalidAmount');
+        const newInvalidPin = document.getElementById('cashoutInvalidPin');
 
 
-    // cashout calculation
+        // cashout calculation
 
-    if (convertedCashoutAmount) {
-        if (convertedCashoutPin === 1234) {
-            subtraction = convertedMainBalance - convertedCashoutAmount;
-            console.log(subtraction);
-            document.getElementById('mainBalance').innerText = subtraction;
+        if (convertedCashoutAmount) {
+            if (convertedCashoutPin === 1234) {
+                subtraction = convertedMainBalance - convertedCashoutAmount;
+                console.log(subtraction);
+                document.getElementById('mainBalance').innerText = subtraction;
+            }
+            else {
+                // alert('put a valid pin number')
+                newInvalidPin.innerText = '*Invalid PIN';
+                newInvalidPin.style.color = 'red'; // Make it red for better visibility
+            }
         }
         else {
-            // alert('put a valid pin number')
-            newInvalidPin.innerText = '*Invalid PIN';
-            newInvalidPin.style.color = 'red'; // Make it red for better visibility
+            // alert('put valid amount')
+            newInvalidAmount.innerText = '*Invalid Amount ';
+            newInvalidAmount.style.color = 'red'; // Make it red for better visibility
         }
-    }
-    else {
-        // alert('put valid amount')
-        newInvalidAmount.innerText = '*Invalid Amount ';
-        newInvalidAmount.style.color = 'red'; // Make it red for better visibility
-    }
 
 
-})
+    })
